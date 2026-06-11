7f7ec0d4886fe29082f7e009c6c3b9f557bb9a9f
FynnKlette <s_klette24@stud.hwr-berlin.de>
2026-06-11 18:52:28 +0200

    first tables

--- db/database_create.py
+++ db/database_create.py
@@ -1,4 +1,33 @@
 import sqlite3
 
-#conn = sqlite3.connect("tauschdaten.db")
-#conn.close()
+'''
+# Create DB
+conn = sqlite3.connect("tauschdaten.db")
+conn.close()
+'''
+
+# DB Connector
+db_path = "tauschdaten.db"
+
+with sqlite3.connect(db_path) as conn:
+    cursor = conn.cursor()
+
+    # Create Tables
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS studiengang (
+        studiengang TEXT NOT NULL PRIMARY KEY
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS studienbüro_ma (
+        ma-id TEXT NOT NULL PRIMARY KEY,
+        vorname TEXT,
+        name TEXT,
+        email TEXT,
+        password TEXT
+    )
+    """)
+
+conn.commit()
+
