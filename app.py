da2988d0ee745dae05ae8a0817c85f8ee89c3383
FynnKlette <s_klette24@stud.hwr-berlin.de>
2026-06-22 23:12:21 +0200

    undo app.py

--- app.py
+++ app.py
@@ -9,16 +9,3 @@ def startapp():
     return render_template("dashboard.html")
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
-from flask import Flask, render_template
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.errorhandler(401)
-def internal_server_error(e):
-    return render_template('401.html'), 401
