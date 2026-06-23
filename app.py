82317a265cc09dd810560674375d8924ffe1bc27
Danial-Banitorfi <s_banitorfi24@stud.hwr-berlin.de>
2026-06-23 09:16:23 +0200

    app correction

--- app.py
+++ app.py
@@ -50,17 +50,6 @@ def internal_server_error(e):
 @app.errorhandler(401)
 def internal_server_error(e):
     return render_template('401.html'), 401
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.errorhandler(401)
-def internal_server_error(e):
-    return render_template('401.html'), 401
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True)
